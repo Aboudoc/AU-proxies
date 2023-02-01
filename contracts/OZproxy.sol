192cb1ad50bedb17cbef61dedafd4f2a983d1450
Aboudoc <aboutika.reda@gmail.com>
2023-02-01 12:32:33 +0100

    openZeppelin Proxy

---
+++ contracts/OZproxy.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/proxy/Proxy.sol";
+
+/** @title A Minimalistic Proxy using Proxy.sol frol OpenZeppelin
+ * @dev This proxy contract is implementing eip1967
+ * We define the  spot to store the implementation by:
+ * keccak256("eip1967.proxy.implementation") - 1
+ * implementation's variables cannot collite with proxy
+ */
+contract SmallProxy is Proxy {
+    // This is the keccak-256 hash of "eip1967.proxy.implementation" subtracted by 1
+    bytes32 private constant _IMPLEMENTATION_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
+
+    function setImplementation(address newImplementation) public {
+        assembly {
+            sstore(_IMPLEMENTATION_SLOT, newImplementation)
+        }
+    }
+
+    /**
+     * @dev This is a virtual function that should be overridden so it returns the address to which the fallback function
+     * and {_fallback} should delegate.
+     */
+    function _implementation() internal view override returns (address implementationAddress) {
+        assembly {
+            implementationAddress := sload(0)
+        }
+    }
+
+    //function to get the the data for the proxy contract's call
+    function getDataToTransact(uint256 _numberToUpdate) public pure returns (bytes memory) {
+        return abi.encodeWithSignature("setValue(uint256)", _numberToUpdate);
+    }
+
+    function readStorage() public view returns (uint256 valueAtStorageSlotZero) {
+        assembly {
+            valueAtStorageSlotZero := sload(0)
+        }
+    }
+}
+
+contract ImplementationA {
+    uint256 public value;
+
+    function setValue(uint256 newValue) public {
+        value = newValue;
+    }
+}
+
+contract ImplementationB {
+    uint256 public value;
+
+    function setValue(uint256 newValue) public {
+        value = newValue + 2;
+    }
+}
